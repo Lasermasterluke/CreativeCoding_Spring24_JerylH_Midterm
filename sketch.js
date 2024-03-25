00a82ad0d8cabf917610da3a811620a4035454e0
laser <lyrej37dxi@yahoo.com>
2024-03-25 01:49:57 -0400

    Make the hill scroll to simulate movement.
    Standardize FPS, added hill scrolling.

--- sketch.js
+++ sketch.js
@@ -5,10 +5,12 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	// initialize hill
 	hill = new Hill();
+	frameRate(60);
 }
 
 function draw() {
 	background(220);
+	hill.update();
 	hill.draw();
 }
 
@@ -20,19 +22,24 @@ class Hill {
 	constructor() {
 		this.detail = 0.005; // detail level of the hill's shape
 		this.steepness = 0.4; // controls the vertical scale of the hill
+		this.offset = 0; // offset to "move" the hill
 	}
 
 	draw() {
 		noFill();
 		beginShape();
-		let startHeight = height * this.steepness; // starting height at the left of the canvas
+		let startHeight = height * this.steepness + (windowHeight / 2); // starting height at the left of the canvas
 		for (let x = 0; x < width; x++) {
 			// dynamically adjust the y-coordinate based on noise and scale it based on the canvas height
-			let y = map(noise(x * this.detail), 0, 1, startHeight, 0) + (windowHeight / 2);
+			let y = map(noise(this.offset + x * this.detail), 0, 3, startHeight, 0);
 			// create an upward slope by decreasing startheight gradually
-			startHeight -= this.steepness;
 			vertex(x, y);
+			startHeight -= this.steepness;
 		}
 		endShape();
+	}
+	
+	update() {
+		this.offset += .005;
 	}
 }
